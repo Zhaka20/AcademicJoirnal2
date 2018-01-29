6877fd7d77a30193696dd6c7679ca6f4d1381370
Olzhas Kashkynbekov <Olzhas_Kashkynbekov@epam.com>
2018-01-29 09:10:45 +0600

    commit from git cmd

---
+++ AcademicJournal.AbstractBLL/AbstractServices/ISubmissionService.cs
@@ -0,0 +1,11 @@
+﻿using AcademicJournal.AbstractBLL.AbstractServices.Common;
+using AcademicJournal.DataModel.Models;
+using AcademicJournal.FrontToBLL_DTOs.DTOs;
+
+namespace AcademicJournal.AbstractBLL.AbstractServices
+{
+    public interface ISubmissionService 
+    {
+        void DeleteFileFromFSandDBIfExists(SubmitFileDTO submitFile);
+    }
+}
